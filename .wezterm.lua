f01c8b57319fc9b403b46e86b09d56da51c422a8
antonlau-1 <antony.hy.lau@gmail.com>
2025-03-23 21:25:31 +0000

    update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,10 +2,12 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("BlexMono Nerd Font Mono")
-config.font_size = 11
-
-config.window_background_opacity = 0.7
-config.macos_window_background_blur = 10
+config = {
+	font = wezterm.font("BlexMono Nerd Font Mono", { weight = "Regular" }),
+	font_size = 11,
+	window_background_opacity = 0.7,
+	macos_window_background_blur = 10,
+	enable_tab_bar = false,
+}
 
 return config
